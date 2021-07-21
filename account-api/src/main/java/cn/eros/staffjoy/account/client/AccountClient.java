f93b58718a69d1db17eda4af99b883f8de979f77
周光兵 <mir3732@live.com>
2021-07-21 13:41:40 +0800

    Define AccountAPI interface

--- account-api/src/main/java/cn/eros/staffjoy/account/client/AccountClient.java
+++ account-api/src/main/java/cn/eros/staffjoy/account/client/AccountClient.java
@@ -1,7 +1,16 @@
 package cn.eros.staffjoy.account.client;
 
 import cn.eros.staffjoy.account.AccountConstant;
+import cn.eros.staffjoy.account.dto.*;
+import cn.eros.staffjoy.common.api.BaseResponse;
+import cn.eros.staffjoy.common.auth.AuthConstant;
+import cn.eros.staffjoy.common.validation.PhoneNumber;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 /**
  * @author 周光兵
@@ -9,4 +18,54 @@ import org.springframework.cloud.openfeign.FeignClient;
  */
 @FeignClient(name = AccountConstant.SERVICE_NAME, path = "/v1/account", url = "${staffjoy.account-service-endpoint")
 public interface AccountClient {
+    @PostMapping(path = "/create")
+    GenericAccountResponse createAccount(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String auth,
+                                         @RequestBody @Valid CreateAccountRequest request);
+
+    @PostMapping(path = "/track_event")
+    BaseResponse trackEvent(@RequestBody @Valid TrackEventRequest request);
+
+    @PostMapping(path = "/sync_user")
+    BaseResponse syncUser(@RequestBody @Valid SyncUserRequest request);
+
+    @GetMapping(path = "/list")
+    ListAccountResponse listAccounts(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String auth,
+                                     @RequestParam int offset,
+                                     @RequestParam @Min(0) int limit);
+
+    @PostMapping(path = "/get_or_create")
+    GenericAccountResponse getOrCreateAccount(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String auth,
+                                              @RequestBody @Valid GetOrCreateRequest request);
+
+    @GetMapping(path = "/get")
+    GenericAccountResponse getAccount(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String auth,
+                                      @RequestParam @NotBlank String userid);
+
+    @PutMapping(path = "/update")
+    GenericAccountResponse updateAccount(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String auth,
+                                         @RequestBody @Valid AccountDto accountDto);
+
+    @GetMapping(path = "/get_account_by_phonenumber")
+    GenericAccountResponse getAccountByPhoneNumber(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String auth,
+                                                   @RequestParam @PhoneNumber String phoneNumber);
+
+    @PutMapping(path = "/update_password")
+    BaseResponse updatePassword(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String auth,
+                                @RequestBody @Valid UpdatePasswordRequest request);
+
+    @PostMapping(path = "/verify_password")
+    GenericAccountResponse verifyPassword(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String auth,
+                                @RequestBody @Valid VerifyPasswordRequest request);
+
+    @PostMapping(path = "/request_password_reset")
+    BaseResponse requestPasswordReset(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String auth,
+                                      @RequestBody @Valid PasswordResetRequest request);
+
+    @PostMapping(path = "/request_email_change")
+    BaseResponse requestEmailChange(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String auth,
+                                    @RequestBody @Valid EmailChangeRequest request);
+
+    @PostMapping(path = "/change_email")
+    BaseResponse changeEmail(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String auth,
+                             @RequestBody @Valid EmailConfirmationRequest request);
 }
