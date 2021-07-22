8f3c503092a708767088fa1e6f461403db074fbf
周光兵 <mir3732@live.com>
2021-07-22 13:02:27 +0800

    add generic properties class

---
+++ common-lib/src/main/java/cn/eros/staffjoy/common/config/StaffjoyProps.java
@@ -0,0 +1,24 @@
+package cn.eros.staffjoy.common.config;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @author 周光兵
+ * @date 2021/7/22 12:54
+ */
+@ConfigurationProperties(prefix = "staffjoy.common")
+@Data
+@Builder
+public class StaffjoyProps {
+    @NotBlank
+    private String sentryDsn;
+    /**
+     * DeployEnvVar is set by Kubernetes during a new deployment so we can identify the code version
+     */
+    @NotBlank
+    private String deployEnv;
+}
