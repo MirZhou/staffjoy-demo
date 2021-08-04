817c642c900bdd31bec9c38a5d32f70403f2e079
周光兵 <mir3732@live.com>
2021-08-04 23:19:45 +0800

    添加JWT生成Token的方法

--- common-lib/src/main/java/cn/eros/staffjoy/common/crypto/Sign.java
+++ common-lib/src/main/java/cn/eros/staffjoy/common/crypto/Sign.java
@@ -1,12 +1,16 @@
 package cn.eros.staffjoy.common.crypto;
 
+import cn.eros.staffjoy.common.error.ServiceException;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.util.StringUtils;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author 周光兵
@@ -18,10 +22,51 @@ public class Sign {
     public static final String CLAIM_SUPPORT = "support";
 
     private static final Map<String, JWTVerifier> VERIFIER_MAP = new HashMap<>();
-    private static Map<String, Algorithm> algorithmMap = new HashMap<>();
+    private static final Map<String, Algorithm> ALGORITHM_MAP = new HashMap<>();
+
+    public static String generateEmailConfirmationToken(String userid, String email, String signingToken) {
+        Algorithm algorithm = getAlgorithm(signingToken);
+
+        return JWT.create()
+            .withClaim(CLAIM_EMAIL, email)
+            .withClaim(CLAIM_USER_ID, userid)
+            .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(2L)))
+            .sign(algorithm);
+    }
+
+    public static String generateSessionToken(String userid, String signingToken, boolean support, long duration) {
+        if (StringUtils.isEmpty(signingToken)) {
+            throw new ServiceException("No signing token present");
+        }
+
+        Algorithm algorithm = getAlgorithm(signingToken);
+
+        return JWT.create()
+            .withClaim(CLAIM_USER_ID, userid)
+            .withClaim(CLAIM_SUPPORT, support)
+            .withExpiresAt(new Date(System.currentTimeMillis() + duration))
+            .sign(algorithm);
+    }
 
     public static DecodedJWT verifySessionToken(String tokenString, String signingToken) {
         return verifyToken(tokenString, signingToken);
+    }
+
+    private static Algorithm getAlgorithm(String signingToken) {
+        Algorithm algorithm = ALGORITHM_MAP.get(signingToken);
+
+        if (algorithm == null) {
+            synchronized (ALGORITHM_MAP) {
+                algorithm = ALGORITHM_MAP.get(signingToken);
+
+                if (algorithm == null) {
+                    algorithm = Algorithm.HMAC512(signingToken);
+                    ALGORITHM_MAP.put(signingToken, algorithm);
+                }
+            }
+        }
+
+        return algorithm;
     }
 
     private static DecodedJWT verifyToken(String tokenString, String signingToken) {
