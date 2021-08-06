9eae24191251014399e43f85941ab7c2d120b814
周光兵 <mir3732@live.com>
2021-08-06 08:37:21 +0800

    add class: CompanyConstant

---
+++ company-api/src/main/java/cn/eros/staffjoy/company/CompanyConstant.java
@@ -0,0 +1,9 @@
+package cn.eros.staffjoy.company;
+
+/**
+ * @author 周光兵
+ * @date 2021/8/6 08:35
+ */
+public class CompanyConstant {
+    public static final String SERVICE_NAME = "company-service";
+}
