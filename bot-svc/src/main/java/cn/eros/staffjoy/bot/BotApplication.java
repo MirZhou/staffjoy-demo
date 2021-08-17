905dcdf7524fe30d1f192e9ab5b9dba49285ae3d
mir37 <mir3732@live.com>
2021-08-17 14:06:23 +0800

    创建启动类

---
+++ bot-svc/src/main/java/cn/eros/staffjoy/bot/BotApplication.java
@@ -0,0 +1,16 @@
+package cn.eros.staffjoy.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>create time：2021-08-17 13:59
+ *
+ * @author Eros
+ */
+@SpringBootApplication
+public class BotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BotApplication.class, args);
+    }
+}
