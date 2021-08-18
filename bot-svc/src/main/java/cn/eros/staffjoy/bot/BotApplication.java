91fc17461d9e67e400c60ad57105ca44f4281d9c
mir37 <mir3732@live.com>
2021-08-18 13:36:05 +0800

    enable feign clients

--- bot-svc/src/main/java/cn/eros/staffjoy/bot/BotApplication.java
+++ bot-svc/src/main/java/cn/eros/staffjoy/bot/BotApplication.java
@@ -2,13 +2,16 @@ package cn.eros.staffjoy.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * <p>create time：2021-08-17 13:59
  *
  * @author Eros
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableFeignClients({"cn.eros.staffjoy.account", "cn.eros.staffjoy.company", "cn.eros.staffjoy.mail"})
 public class BotApplication {
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
