d8efab3207a222b0f932acd7295095c56141376d
Eros <mir3732@live.com>
2024-05-10 22:57:39 +0800

    为异步线程复制主线程上下文

---
+++ common-lib/src/main/java/cn/eros/staffjoy/common/async/ContextCopyingDecorator.java
@@ -0,0 +1,24 @@
+package cn.eros.staffjoy.common.async;
+
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+/**
+ * @author Eros
+ * @since 2024/5/10
+ */
+public class ContextCopyingDecorator implements TaskDecorator {
+    @Override
+    public Runnable decorate(Runnable runnable) {
+        RequestAttributes context = RequestContextHolder.currentRequestAttributes();
+        return () -> {
+            try {
+                RequestContextHolder.setRequestAttributes(context);
+                runnable.run();
+            } finally {
+                RequestContextHolder.resetRequestAttributes();
+            }
+        };
+    }
+}
